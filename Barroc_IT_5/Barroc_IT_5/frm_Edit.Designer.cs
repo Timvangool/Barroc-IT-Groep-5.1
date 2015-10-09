fb56deab6fb2ff5e749e228d69fa084f11a50bbd
Timvangool <timvangool@hotmail.com>
2015-10-09 09:19:28 +0200

    08-10-2015 Added all forms
    All forms added

--- Barroc_IT_5/Barroc_IT_5/frm_Edit.Designer.cs
+++ Barroc_IT_5/Barroc_IT_5/frm_Edit.Designer.cs
@@ -28,11 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cb_Customers = new System.Windows.Forms.ComboBox();
+            this.btn_SelectCustomer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cb_Customers
+            // 
+            this.cb_Customers.FormattingEnabled = true;
+            this.cb_Customers.Location = new System.Drawing.Point(12, 12);
+            this.cb_Customers.Name = "cb_Customers";
+            this.cb_Customers.Size = new System.Drawing.Size(216, 21);
+            this.cb_Customers.TabIndex = 0;
+            // 
+            // btn_SelectCustomer
+            // 
+            this.btn_SelectCustomer.Location = new System.Drawing.Point(235, 13);
+            this.btn_SelectCustomer.Name = "btn_SelectCustomer";
+            this.btn_SelectCustomer.Size = new System.Drawing.Size(75, 23);
+            this.btn_SelectCustomer.TabIndex = 1;
+            this.btn_SelectCustomer.Text = "Select";
+            this.btn_SelectCustomer.UseVisualStyleBackColor = true;
+            // 
+            // frm_Edit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "frm_Edit";
+            this.ClientSize = new System.Drawing.Size(325, 47);
+            this.Controls.Add(this.btn_SelectCustomer);
+            this.Controls.Add(this.cb_Customers);
+            this.Name = "frm_Edit";
+            this.Text = "Barroc_IT";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cb_Customers;
+        private System.Windows.Forms.Button btn_SelectCustomer;
     }
 }
